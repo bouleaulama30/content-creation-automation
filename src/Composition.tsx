964a6bf0c97d0723f6cf67899d4f492e6223f8b0
leo <leo.lefebvre@telecom-paris.fr>
2026-03-01 19:02:12 -0500

    first commit

---
+++ src/Composition.tsx
@@ -0,0 +1,26 @@
+import {AbsoluteFill, Html5Audio, Img, staticFile} from 'remotion';
+
+export const Video: React.FC = () => {
+  return (
+    <AbsoluteFill>
+      <Img src={staticFile('lion.png')} />
+      <Html5Audio src={staticFile('audio.mp3')} />
+    </AbsoluteFill>
+  );
+};
+
+export const BackgroundImage: React.FC = () => {
+  return (
+    <AbsoluteFill>
+      <Img src={staticFile('lion.png')} />
+    </AbsoluteFill>
+  );
+};
+
+export const AudioVideo = () => {
+  return (
+    <AbsoluteFill>
+      <Html5Audio src={staticFile('audio.mp3')} />
+    </AbsoluteFill>
+  );
+};
---
+++ src/Root.tsx
@@ -0,0 +1,36 @@
+import "./index.css";
+import { Composition } from "remotion";
+import {Video, BackgroundImage, AudioVideo } from "./Composition";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="Video"
+        component={Video}
+        durationInFrames={600}
+        fps={60}
+        width={1080}
+        height={1920}
+      />
+
+      <Composition
+        id="BackgroundImage"
+        component={BackgroundImage}
+        durationInFrames={60}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+
+      <Composition
+        id="Audio"
+        component={AudioVideo}
+        durationInFrames={60}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+    </>
+  );
+};
